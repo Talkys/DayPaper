cabb6fde5ae72fff8676fe7e5bec074b4fc72045
Tallys <tallys.assis@gmail.com>
2021-11-10 18:19:03 -0300

    Arquivos de execução

---
+++ set.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+./sel -3 "Caminho da pasta dos wallpapers" "Caminho da pasta do now.png"
+
+#Detalhe, tem que tirar as aspas e deixar só o caminho, mas acho que isso não vai bugar em todos os sistemas, depende de como o bash se comporta aí caso vc tenha o configurado
